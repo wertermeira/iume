e59e53ce667c65ac5c0dda8ef265870ec7542ceb
Werter Meira <wertermeira@gmail.com>
2020-09-24 11:50:58 -0300

    hot fix included

--- app/controllers/v1/restaurants_controller.rb
+++ app/controllers/v1/restaurants_controller.rb
@@ -5,7 +5,7 @@ module V1
 
     def show
       render json: @resturant, serializer: V1::Public::RestaurantSerializer, status: :ok,
-             include: included_serializer, scope: { current_user: current_user }
+             include: params[:included], scope: { current_user: current_user }
     end
 
     private
@@ -23,12 +23,6 @@ module V1
 
     def preview
       params[:preview].present?
-    end
-
-    def included_serializer
-      return params[:included] if request.get?
-
-      'sections,phones,address,address.city,address.city.state'
     end
   end
 end
