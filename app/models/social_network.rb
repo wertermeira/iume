d44b696f121bd9866fc2a4529618105681491b2c
Werter Meira <wertermeira@gmail.com>
2020-10-22 09:43:59 -0300

    add notification

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -6,4 +6,14 @@ class SocialNetwork < ApplicationRecord
   validates :restaurant_id, uniqueness: { scope: :provider }
   validates :username, :provider, presence: true
   validates :username, length: { maximum: 200 }, allow_blank: true
+
+  after_create :notification_slack
+
+  private
+
+  def notification_slack
+    message = "Olá! <#{ENV['FRONTEND_URL']}/qr/#{restaurant.uid}|#{restaurant.name}> "\
+              "acabou de adicionar o #{provider} <https://#{provider}.com/#{username}|#{username}> :bowtie:"
+    SlackNotifyJob.perform_later(message: message, channel: ENV.fetch('SLACK_NOTIFY_CHANNEL', '#iume-notifications'))
+  end
 end
